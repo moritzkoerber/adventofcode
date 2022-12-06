78b5c1a5add27e5f5d1a1c248bd32e6809192b49
moritzkoerber <koerber.moritz@gmail.com>
2022-12-06 22:46:02 +0100

    Day 6

---
+++ 2022/day6/day6.py
@@ -0,0 +1,21 @@
+# Python
+## Part 1
+with open("data.txt") as f:
+    signal = f.read()
+
+
+def search_signal(signal_string: str, signal_length: int) -> int:
+    c = signal_string[: signal_length - 1]
+    for i in signal_string[signal_length - 1 :]:
+        if i in c:
+            c = c[c.index(i) + 1 :] + i
+            continue
+        c += i
+        if len(set(c)) == signal_length:
+            return signal_string.index(c) + signal_length
+
+
+print(search_signal(signal, 4))
+
+## Part 2
+print(search_signal(signal, 14))
