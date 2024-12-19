1a258ae5b86553c67b72d92c18301a327f798267
Moritz Körber <koerber.moritz@gmail.com>
2024-12-19 23:00:12 +0100

    2024-day05

--- 2022/day02/day2.py
+++ 2022/day02/day2.py
@@ -4,7 +4,9 @@ with open("data.txt") as f:
     games = f.read().replace(" ", "").splitlines()
 
 outcomes_numeric = [(ord(i) - 64, ord(j) - 64 - 23) for i, j in games]
+
 lookup_dict = {-2: 1, 2: -1}
+
 print(sum((lookup_dict.get(j - i, j - i) + 1) * 3 + j for i, j in outcomes_numeric))
 
 ## Part 2
