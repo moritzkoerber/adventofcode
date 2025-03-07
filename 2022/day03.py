0dadb61abbe43df448cf27ac4e6d71ed9f9397cf
Moritz Körber <moritz.koerber@zeiss.com>
2025-03-07 07:45:16 +0100

    fixup! Day 3

--- 2022/day03.py
+++ 2022/day03.py
@@ -1,29 +1,23 @@
 # Python
 ## Part 1
-from functools import reduce
-from itertools import chain
-from typing import Iterable
-
 with open("data.txt") as f:
-    items = f.read().splitlines()
+    compartments = f.read().splitlines()
 
 
-def count_overlap(iterable: Iterable) -> int:
-    return sum(
-        ord(letter) - 38 - (58 * letter.islower())
-        for letter in chain.from_iterable(
-            [
-                reduce(
-                    lambda x, y: set(x) & set(y),
-                    i,
-                )
-                for i in iterable
-            ]
-        )
-    )
+def count_overlap(comp1: str, *comp2: tuple[str, ...]) -> int:
+    letter = (set(comp1).intersection(*comp2)).pop()
+    return ord(letter) - 38 - 58 * letter.islower()
 
 
-print(count_overlap([(i[: (len(i) // 2)], i[(len(i) // 2) :]) for i in items]))
+print(
+    sum(count_overlap(*(i[: (len(i) // 2)], i[(len(i) // 2) :])) for i in compartments)
+)
+
 
 ## Part 2
-print(count_overlap([items[i : i + 3] for i in range(0, len(items), 3)]))
+print(
+    sum(
+        count_overlap(*(compartments[i : i + 3]))
+        for i in range(0, len(compartments), 3)
+    )
+)
