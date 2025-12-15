c10d5472094258d39a956f2af446fdcd409c3f92
Moritz Körber <koerber.moritz@gmail.com>
2025-12-15 15:07:50 +0100

    Day 5

---
+++ 2025/day05.py
@@ -0,0 +1,25 @@
+# Part 1
+import pathlib
+
+ranges, ids = pathlib.Path("data.txt").read_text().split("\n\n")
+
+valid_ids = [[*map(int, p.split("-"))] for p in ranges.splitlines()]
+
+
+print(
+    sum(
+        any(
+            int(ingredient_id) >= ll and int(ingredient_id) <= ul
+            for ll, ul in valid_ids
+        )
+        for ingredient_id in ids.splitlines()
+    )
+)
+
+# Part 2
+curr_max, total = 0, 0
+for ll, ul in sorted(valid_ids):
+    total += max(curr_max, ul + 1) - max(curr_max, ll)
+    curr_max = max(curr_max, ul + 1)
+
+print(total)
