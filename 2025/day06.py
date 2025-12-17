8232fc64093f1b38692bfec28a605639a5ae827a
Moritz Körber <koerber.moritz@gmail.com>
2025-12-17 22:25:29 +0100

    Day 6

---
+++ 2025/day06.py
@@ -0,0 +1,22 @@
+# Part 1
+import pathlib
+
+p_input = pathlib.Path("data.txt").read_text().splitlines()
+*nums, ops = map(str.split, p_input)
+
+total = 0
+for op, num_col in zip(ops, zip(*nums)):
+    total += eval(op.join(map(str, num_col)))
+print(total)
+
+# Part 2
+total = ""
+for *num_col, op in zip(*p_input):
+    if op.strip():
+        t_op = op
+    if num := "".join(num_col).strip():
+        total += num + t_op
+    else:
+        total = total[:-1] + "+"
+
+print(eval(total[:-1]))
